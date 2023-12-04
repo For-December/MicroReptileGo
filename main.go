b520f8af40008d87d23a35c1a90782ccc726b6e7
forDecember <1921567337@qq.com>
2023-12-04 19:57:03 +0800

    update: 一键启动bat+api

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/tebeka/selenium"
 	"log"
+	"os"
+	"os/exec"
 )
 
 var driver selenium.WebDriver
 
 func main() {
 	log.SetFlags(log.Lshortfile | log.Ldate | log.Ltime)
+
+	// 启动 selenium server
+	// 获取当前目录
+	currentDir, err := os.Getwd()
+	if err != nil {
+		fmt.Println("无法获取当前目录：", err)
+		return
+	}
+	batFilePath := currentDir + "\\resources\\run.bat"
+	cmd := exec.Command("cmd.exe", "/C", batFilePath)
+	// 也可以直接相对路径".\\resources\\run.bat"
+	err = cmd.Start()
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
 	router := gin.Default()
 	biliBiliSummary(router)
 	webScreen(router)
--- resources/run.bat
+++ resources/run.bat
@@ -1,1 +1,2 @@
-java -jar selenium-server-4.15.0.jar standalone
+cd C:\Users\forDece\GolandProjects\MicroReptileGo\resources\
+start java -jar selenium-server-4.15.0.jar standalone
